9c7ddb38ffffc8ba41ca078c2a4bc9e0533f6f2b
Niels de Vries <info@nielsdevries.net>
2018-09-03 11:51:05 +1000

    add Loading component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+
+library.add(faSpinner);
 
 const DEFAULT_QUERY = "redux";
 const DEFAULT_HPP = "100";
@@ -30,7 +35,8 @@ class App extends Component {
       results: null,
       searchKey: "",
       searchTerm: DEFAULT_QUERY,
-      error: null
+      error: null,
+      isLoading: false
     };
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -54,11 +60,16 @@ class App extends Component {
       results: {
         ...results,
         [searchKey]: { hits: updatedHits, page }
-      }
+      },
+      isLoading: false
     });
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({
+      isLoading: true
+    });
+
     axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
@@ -104,7 +115,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey, error } = this.state;
+    const { searchTerm, results, searchKey, error, isLoading } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -129,11 +140,15 @@ class App extends Component {
           <Table list={list} onDismiss={this.onDismiss} />
         )}
         <div className="interactions">
-          <Button
-            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
-          >
-            Meer
-          </Button>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <Button
+              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+            >
+              Meer
+            </Button>
+          )}
         </div>
       </div>
     );
@@ -194,6 +209,8 @@ const Button = ({ onClick, className, children }) => (
     {children}
   </button>
 );
+
+const Loading = () => <FontAwesomeIcon icon="spinner" />;
 
 Button.defaultProps = {
   className: ""
